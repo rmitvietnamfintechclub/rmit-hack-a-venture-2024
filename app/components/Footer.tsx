6b94a8df81f0a08a04f27d9dc5a567e960067d82
Pham Nhat Minh <s4019811@rmit.edu.vn>
2024-09-29 02:29:52 +0700

    tony done navbar and footer, need to double check

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,151 @@
-import React from 'react'
+import React from "react";
+import { TiSocialFacebook } from "react-icons/ti";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { MdEmail } from "react-icons/md";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-[50px] px-[20px] border-t-[1px] border-solid border-[#1F2937] flex flex-col gap-[80px] lg:items-center">
+      <section className="w-full flex flex-col gap-[50px] lg:flex-row lg:justify-around">
+      <SocialMediaList />
+      <OrganizerList />
+      <ContactList />
+      </section>
+      <div className="text-[#9da3af]">© Hack-a-thon 2024, All Rights Reserved</div>
+    </footer>
+  );
+};
+
+const SocialMediaList = () => {
+  return (
+    <div className="flex flex-col gap-[20px]">
+      {/*--------- text effect ---------*/}
+      <div className="relative">
+        <h2 className="inline-block text-[#C93FDD] text-[18px] font-semibold">
+          Our Socials
+        </h2>
+        <span
+          className="absolute bottom-0 left-0 w-[120px] h-[2px]"
+          style={{
+            background:
+              "linear-gradient(to right, #a64ce0, rgba(166, 76, 224, 0.5), transparent)",
+          }}
+        ></span>
+      </div>
+
+      {/*---------------------------- icon link section ---------------------*/}
+      <div className="flex gap-[20px]">
+        <a
+          href=""
+          className="w-[40px] h-[40px] bg-white rounded-full flex items-center justify-center"
+        >
+          <TiSocialFacebook className="w-full h-full text-black" />
+        </a>
+        <a
+          href=""
+          className="w-[40px] h-[40px] bg-white rounded-full flex items-center justify-center"
+        >
+          <IoLogoInstagram className="w-[80%] h-[80%] text-black" />
+        </a>
+        <a
+          href=""
+          className="w-[40px] h-[40px] bg-white rounded-full flex items-center justify-center"
+        >
+          <FaLinkedinIn className="w-[50%] h-[50%] text-black" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const OrganizerList = () => {
+  return (
+    <div className="flex flex-col gap-[20px]">
+      {/*--------- text effect ---------*/}
+      <div className="relative">
+        <h2 className="inline-block text-[#C93FDD] text-[18px] font-semibold">
+          Organisers
+        </h2>
+        <span
+          className="absolute bottom-0 left-0 w-[120px] h-[2px]"
+          style={{
+            background:
+              "linear-gradient(to right, #a64ce0, rgba(166, 76, 224, 0.5), transparent)",
+          }}
+        ></span>
+      </div>
+
+      {/*---------------------------- organizers list section ---------------------*/}
+      <div className="flex flex-col gap-[20px]">
+        <div className="flex gap-[15px] items-center">
+          <img
+            className="w-[60px] h-[60px]"
+            src="/alignVerticalLogo.svg"
+            alt="fintech logo"
+          />
+          <h2 className="text-[18px]">RMIT Vietnam Fintech Club</h2>
+        </div>
+
+        <div className="flex gap-[15px] items-center">
+          <img
+            className="w-[60px] h-[60px]"
+            src="/alignVerticalLogo.svg"
+            alt="fintech logo"
+          />
+          <h2 className="text-[18px]">chainLink</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+const ContactList = () => {
+  return (
+    <div className="flex flex-col gap-[20px]">
+      {/*--------- text effect ---------*/}
+      <div className="relative">
+        <h2 className="inline-block text-[#C93FDD] text-[18px] font-semibold">
+          Contacts
+        </h2>
+        <span
+          className="absolute bottom-0 left-0 w-[95px] h-[2px]"
+          style={{
+            background:
+              "linear-gradient(to right, #a64ce0, rgba(166, 76, 224, 0.5), transparent)",
+          }}
+        ></span>
+      </div>
+
+      {/*---------------------------- contact list section ---------------------*/}
+      <div className="flex flex-col gap-[20px]">
+        <div className="flex gap-[15px] items-start lg:items-center">
+          <BiSolidPhoneCall className="text-[24px] mt-[5px] lg:m-0" />
+          <div className="text-[16px]">
+            <div>
+              <span> Nguyen Manh Dung </span>
+              <span className="text-[#9da3af] hidden lg:inline"> - </span>
+              <span className="text-[#9da3af] block lg:inline">Co-project leader</span>
+            </div>
+            <p>03535343434</p>
+          </div>
+        </div>
+        <div className="flex gap-[15px] items-start lg:items-center">
+          <BiSolidPhoneCall className="text-[24px] mt-[5px] lg:m-0" />
+          <div className="text-[16px]">
+            <div>
+              <span>Hoang Nguyen Nhat Minh</span>
+              <span className="text-[#9da3af] hidden lg:inline"> - </span>
+              <span className="text-[#9da3af] block lg:inline">Co-project leader</span>
+            </div>
+            <p>03535343434</p>
+          </div>
+        </div>
+        <div className="flex gap-[15px] items-center">
+          <MdEmail className="text-[24px]" />
+          <p className="text-[16px]">rmitfintech123@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,162 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
 
 export const Header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <>
+      <LaptopNav />
+      <MobileNav />
+    </>
+  );
+};
+
+const LaptopNav = () => {
+  const [hoverItemNumber, setHoverItemNumber] = useState(-1);
+  return (
+    <nav
+      id="header"
+      className="sticky top-0 w-full h-[72px] backdrop-blur-sm bg-black/30 border-b-[1px] border-solid border-[#374051] text-[14px] hidden lg:flex justify-between items-center gap-[100px]"
+    >
+      {/*-----------------------------left side ------------------------*/}
+      <div className=" lg:ms-[70px] h-[100%] w-[650px] flex justify-between items-end items-center">
+        <a className="no-underline block" href="#">
+          <img
+            className="w-[60px] h-[60px]"
+            src="/alignVerticalLogo.svg"
+            alt="laptop logo"
+          />
+        </a>
+        <a
+          className={`hover:text-white text-[#9CA3AF] font-normal  cursor-pointer`}
+          style={{
+            textShadow:
+              hoverItemNumber == 1 ? "1px 1px 5px rgba(201, 63, 221, 1)" : "",
+          }}
+          onMouseEnter={() => setHoverItemNumber(1)}
+          onMouseLeave={() => setHoverItemNumber(-1)}
+        >
+          <h1>About Hack-A-Venture</h1>
+        </a>
+        <a
+          className={`hover:text-white text-[#9CA3AF] font-normal  cursor-pointer`}
+          style={{
+            textShadow:
+              hoverItemNumber == 2 ? "1px 1px 5px rgba(201, 63, 221, 1)" : "",
+          }}
+          onMouseEnter={() => setHoverItemNumber(2)}
+          onMouseLeave={() => setHoverItemNumber(-1)}
+        >
+          <h1>Rules & Regulations</h1>
+        </a>
+        <a
+          className={`hover:text-white text-[#9CA3AF] font-normal  cursor-pointer`}
+          style={{
+            textShadow:
+              hoverItemNumber == 3 ? "1px 1px 5px rgba(201, 63, 221, 1)" : "",
+          }}
+          onMouseEnter={() => setHoverItemNumber(3)}
+          onMouseLeave={() => setHoverItemNumber(-1)}
+        >
+          <h1>Our Sponsors & Partners</h1>
+        </a>
+      </div>
+
+      {/*-----------------------------right side ------------------------*/}
+      <div className="lg:me-[70px]  h-full w-[320px] flex justify-between items-center">
+        <a href="" className="no-underline font-semibold  text-[#C93FDD]">
+          See Booklet
+        </a>
+        <a href="" className="no-underline font-semibold  text-[#C93FDD]">Contact Us</a>
+        <button className="px-[15px] py-[5px] rounded-2xl bg-[#B25FD6] text-white">
+          Join Now 
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+const MobileNav = () => {
+  const [hamburgerBarIsActive, setHamburgerBarIsActive] = useState(false);
+  const [hoverItemNumber, setHoverItemNumber] = useState(-1);
+
+  return (
+    <section className="lg:hidden flex justify-between items-center  sticky top-0 z-50">
+      <nav className=" px-[30px] w-full h-[72px] backdrop-blur-sm bg-black/30 border-b-[1px] border-solid border-[#374051] flex justify-between items-center">
+        {/*---------- mobile logo ----------*/}
+        <a className="no-underline block" href="#header">
+          <img className="h-[40px]" src="/alignHorizontalLogo.svg" alt="mobile logo" />
+        </a>
+
+        {/*---------- hamburger bar --------*/}
+        <div
+          onClick={() => {
+            setHamburgerBarIsActive(!hamburgerBarIsActive);
+          }}
+          className="flex flex-col justify-around w-[40px] h-[30px] cursor-pointer"
+        >
+          <div className="w-full h-[3px] bg-white" />
+          <div
+            className={`${
+              hamburgerBarIsActive && "hidden"
+            } w-full h-[3px] bg-white`}
+          />
+          <div className="w-full h-[3px] bg-white" />
+        </div>
+      </nav>
+      {/*----------------------- navbar body ---------------*/}
+      <section
+        className={`absolute top-[72px] w-full h-screen mx-auto ${
+          hamburgerBarIsActive ? "left-0" : "left-full hidden"
+        } top-[75px] bg-black z-50 duration-500 text-[25px]`}
+      >
+        <div className=" my-[30px] mx-auto w-[90%] flex flex-col gap-[50px]">
+          <a
+            className={`hover:text-white text-[#9CA3AF] font-normal  cursor-pointer`}
+            style={{
+              textShadow:
+                hoverItemNumber == 1 ? "1px 1px 5px rgba(201, 63, 221, 1)" : "",
+            }}
+            onMouseEnter={() => setHoverItemNumber(1)}
+            onMouseLeave={() => setHoverItemNumber(-1)}
+          >
+            <h1>About Hack-A-Venture</h1>
+          </a>
+          <a
+            className={`hover:text-white text-[#9CA3AF] font-normal  cursor-pointer`}
+            style={{
+              textShadow:
+                hoverItemNumber == 2 ? "1px 1px 5px rgba(201, 63, 221, 1)" : "",
+            }}
+            onMouseEnter={() => setHoverItemNumber(2)}
+            onMouseLeave={() => setHoverItemNumber(-1)}
+          >
+            <h1>Rules & Regulations</h1>
+          </a>
+          <a
+            className={`hover:text-white text-[#9CA3AF] font-normal  cursor-pointer`}
+            style={{
+              textShadow:
+                hoverItemNumber == 3 ? "1px 1px 5px rgba(201, 63, 221, 1)" : "",
+            }}
+            onMouseEnter={() => setHoverItemNumber(3)}
+            onMouseLeave={() => setHoverItemNumber(-1)}
+          >
+            <h1>Our Sponsors & Partners</h1>
+          </a>
+
+          {/*-----------------------------right side ------------------------*/}
+          <a href="" className="no-underline font-semibold  text-[#C93FDD] block cursor-pointer">
+            See Booklet
+          </a>
+          <a href="" className="no-underline font-semibold  text-[#C93FDD] block cursor-pointer">
+            Contact Us
+          </a>
+          <button className="px-[15px] py-[2px] rounded-2xl bg-[#B25FD6] text-white">
+            Join Now 
+          </button>
+        </div>
+      </section>
+    </section>
+  );
+};

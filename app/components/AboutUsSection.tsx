bc7ef2a7d3dd301661eafabc9c48a682d76a3be2
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-10-06 20:42:57 +0700

    Restructure Judge Section and fix opacity error

--- app/components/AboutUsSection.tsx
+++ app/components/AboutUsSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const AboutUsSection = () => {
 	return (
-		<div className='items-center justify-center mx-auto w-fit max-md:mx-[16px] max-md:mt-[40px] text-lg font-medium text-md md:mx-[280px] '>
+		<div className='items-center justify-center mx-auto w-fit max-md:mx-[16px] max-md:mt-[40px] text-lg font-medium text-md md:mx-[10vw] '>
 			<div>
 				This competition aims to bridge the gap between technical expertise and business strategy by encouraging students to leverage cutting-edge technologies, such as <span className='text-[#C93FDD]'> Artificial Intelligence</span>,
 				<span className='text-[#C93FDD]'> Blockchain</span> and <span className='text-[#C93FDD]'>Cybersecurity</span>, to create scalable and impactful solutions.

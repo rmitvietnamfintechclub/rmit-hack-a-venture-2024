582f9c9ad65157e9b10f2071d8ba75131395b2c2
Logan <phamphuocsang5850@gmail.com>
2024-10-06 21:04:43 +0700

    fix ui

--- app/components/AboutClub.tsx
+++ app/components/AboutClub.tsx
@@ -4,8 +4,15 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import '../css/AboutClub.css';
+import CountUp from "react-countup";
+import { useInView } from 'react-intersection-observer';
 
 export const AboutClub = () => {
+	const { ref, inView } = useInView({
+		triggerOnce: true,
+		threshold: 0.5  // Adjust threshold as needed
+	});
+
 	const settings = {
 		dots: false,
 		arrows: false,
@@ -100,32 +107,68 @@ export const AboutClub = () => {
 			</div >
 			<div className="grid grid-cols-3 md:px-[10vw] md:mt-[60px] max-md:mt-[30px] justify-items-center items-center font-sans max-md:px-[16px]">
 				<div className="flex flex-col items-center max-md:px-[16px] max-md:justify-around max-md:h-full">
-					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">80+</span>
+					<span ref={ref} className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">
+						<CountUp start={0} end={inView ? 80 : 0} duration={5}>
+							{({ countUpRef }) => (
+								<span ref={countUpRef} />
+							)}
+						</CountUp>+
+					</span>
 					<span className="text-lg font-sans text-center text-[#9CA3AF]">Club Members</span>
 				</div>
 				<div className="flex flex-col items-center border-r-[1.5px] border-l-[1.5px] border-[#582768] md:px-[134px] md:py-[42px] max-md:px-[16px] max-md:justify-around max-md:h-full drop-shadow-container">
-					<span className="text-gradient md:text-6xl text-3xl font-semibold font-sans drop-shadow-text">20+</span>
+					<span className="text-gradient md:text-6xl text-3xl font-semibold font-sans drop-shadow-text">
+						<CountUp start={0} end={inView ? 20 : 0} duration={5}>
+							{({ countUpRef }) => (
+								<span ref={countUpRef} />
+							)}
+						</CountUp>+
+					</span>
 					<span className="text-lg font-sans text-center text-[#FBF6FD] md:mt-[32px] justify-center">Industry Partners</span>
 				</div>
 				<div className="flex flex-col items-center max-md:justify-around max-md:h-full max-md:px-[16px]">
-					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">50+</span>
+					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">
+						<CountUp start={0} end={inView ? 50 : 0} duration={5}>
+							{({ countUpRef }) => (
+								<span ref={countUpRef} />
+							)}
+						</CountUp>+
+					</span>
 					<span className="text-lg font-sans text-center text-[#9CA3AF]">Club Projects</span>
 				</div>
 			</div>
 			<div className="grid grid-cols-3 md:px-[10vw] md:mt-[100px] max-md:mt-[30px] justify-items-center items-center font-sans max-md:px-[16px]">
 				<div className="flex flex-col items-center max-md:px-[16px] max-md:justify-around max-md:h-full">
-					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">4X</span>
+					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">
+						<CountUp start={0} end={inView ? 4 : 0} duration={5}>
+							{({ countUpRef }) => (
+								<span ref={countUpRef} />
+							)}
+						</CountUp>X
+					</span>
 					<span className="text-lg font-sans text-center text-[#9CA3AF]">Best Club Of Semester</span>
 				</div>
 				<div className="flex flex-col items-center border-r-[1.5px] border-l-[1.5px] border-[#582768] md:px-[100px] md:py-[42px] max-md:px-[16px] justify-around h-full drop-shadow-container">
-					<span className="text-gradient md:text-6xl text-3xl font-semibold font-sans drop-shadow-text">300M+</span>
+					<span className="text-gradient md:text-6xl text-3xl font-semibold font-sans drop-shadow-text">
+						<CountUp start={0} end={inView ? 300 : 0} duration={5}>
+							{({ countUpRef }) => (
+								<span ref={countUpRef} />
+							)}
+						</CountUp>M+
+					</span>
 					<span className="text-lg font-sans text-center text-[#FBF6FD] md:mt-[32px] justify-center">Fund Raised For Projects</span>
 				</div>
 				<div className="flex flex-col items-center max-md:px-[16px] max-md:justify-around max-md:h-full">
-					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">5000+</span>
+					<span className="text-[#C93FDD] md:text-4xl text-2xl font-semibold font-sans">
+						<CountUp start={0} end={inView ? 5000 : 0} duration={5}>
+							{({ countUpRef }) => (
+								<span ref={countUpRef} />
+							)}
+						</CountUp>+
+					</span>
 					<span className="text-lg font-sans text-center text-[#9CA3AF]">Social Media Followings</span>
 				</div>
-			</div>	
+			</div>
 
 			<div className="md:px-[10vw] max-md:px-[16px] md:mt-[80px]">
 				<h1 className="max-md:hidden md:text-6xl text-3xl font-bold font-sans text-center md:mb-[48px] bg-gradient-to-r from-white via-white to-transparent text-clip"
@@ -219,6 +262,6 @@ export const AboutClub = () => {
 					Through various initiatives such as workshops, events, training programs, and competitions like Hack-A-Venture, the club empowers students to explore the future of financial technology and make a meaningful impact in the community.
 				</p>
 			</div>
-		</div>
+		</div >
 	)
 }
